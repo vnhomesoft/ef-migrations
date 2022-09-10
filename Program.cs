a940687c3886f37d7fe9d97b4f29754893bd89c6
devlife <vnhomesoft@gmail.com>
2022-09-10 23:34:04 +0700

    Multiple env. configurations; auto migration in dev enviroment

--- Program.cs
+++ Program.cs
@@ -22,6 +22,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+// Apply pending migrations automatically in development environment
+// For production env., please use bundle feature (or CLI command)
+if(app.Environment.IsDevelopment()){
+    using (var scope = app.Services.CreateScope())
+    {
+        var dataContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        // Strategy 1: Apply migrations directly
+        dataContext.Database.MigrateAsync();
+
+        // Strategy 2: Apply migrations from generated sql file
+        // (Do not use ExecuteSqlRawAsync here, it will cause connection error)
+        //dataContext.Database.ExecuteSqlRaw(File.ReadAllText("Data/migrations.sql"));
+    }
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 - Student - Subject : n-n
 - Class -
 - Student - Mark : 1-n
+
+### Migrations
+- Development env. : Changes is apply automatically
+- Staging, Production env. : use CLI command to apply change and integrate with CI tool
+```
+# Skip build
+dotnet ef database update -v --no-build
+```
